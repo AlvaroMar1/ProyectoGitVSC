0947a7422bae23e48e51a658893161a13a0f28e2
Álvaro Marqués <alvmarmor6@alu.edu.gva.es>
2026-01-15 13:01:43 +0100

    clase saludo creada desde rama Saludo

---
+++ Saludo.java
@@ -0,0 +1,8 @@
+public class Saludo {
+  public static saludo(){
+    System.out.println("Hola, buenas tardes");
+  }
+  public static despedida(){
+    System.out.println("Hasta la proxima");
+  }
+}
