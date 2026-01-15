125be4e19242c9d7a3680f0020212c4d18d05341
Álvaro Marqués <alvmarmor6@alu.edu.gva.es>
2026-01-15 13:05:50 +0100

    Delete Saludo.java

--- Saludo.java
+++
@@ -1,8 +0,0 @@
-public class Saludo {
-  public static saludo(){
-    System.out.println("Hola, buenas tardes");
-  }
-  public static despedida(){
-    System.out.println("Hasta la proxima");
-  }
-}
